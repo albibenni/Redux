5ccc13829aeb43454ad242f3e8b9900eaae8f136
U0l7270 <alberto.benatti@esternibisp.com>
2022-03-18 14:22:52 +0100

    toolkit

--- ReduxProject/src/components/Auth.js
+++ ReduxProject/src/components/Auth.js
@@ -1,19 +1,26 @@
-import classes from './Auth.module.css';
+import classes from "./Auth.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store/index";
 
 const Auth = () => {
+  const loginHandler = (event) => {
+    event.preventDefault();
+    dispatchEvent(authActions.login());
+  };
+
   return (
     <main className={classes.auth}>
       <section>
         <form>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
           </div>
-          <button>Login</button>
+          <button onClick={login}>Login</button>
         </form>
       </section>
     </main>
